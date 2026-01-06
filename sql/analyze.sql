b570a7c2ad443e1197306fb1cde0d9ef5d6baa1f
AdamLazil <lizaladam@seznam.cz>
2026-01-06 18:20:10 +0100

    updating sql script

--- sql/analyze.sql
+++ sql/analyze.sql
@@ -4,9 +4,24 @@ select * from rastr r ;
 select * from sell_sortiment ss ;
 select * from cash_register cr ;
 
+
+create view time_income as (
 select date,
-	   sum(value),
+	   sum(value) as income,
 	   to_char(date, 'day') as weekDay
 from sell_sortiment ss 
 group by date
-order by date desc;
+order by date desc);
+
+drop view time_income;
+
+select * from time_income;
+	 
+-- according weekday
+select 	
+     sum(income),
+     weekday
+from time_income ti
+group by weekday
+order by sum(income) ;
+     
